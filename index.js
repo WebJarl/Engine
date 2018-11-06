d4a66c50d6c808ab597261bd12969db00dd6ec7d
Ivan Rubinson <soryy708@gmail.com>
2018-11-06 23:39:27 +0200

    Fixup

--- index.js
+++ index.js
@@ -36,125 +36,6 @@ var gameEngine = (function() {
     }
 
     return new function() {
-        this.renderer = function() {
-            var renderables = new Array();
-
-            return new function() {
-                this.createRenderable = function(type, data) {
-                    var renderable = {};
-                    renderable.x = 0;
-                    renderable.y = 0;
-                    renderable.type = type;
-                    
-                    if(type === 'RECTANGLE') {
-                        renderable.w = 0;
-                        renderable.h = 0;
-
-                    } else if(type === 'TEXT') {
-                        ;
-                    } else if(type === 'TILE') {
-                        if(data) {
-                            renderable.tileset = data.tileset;
-                            renderable.sprite = 0;
-                            renderable.w = data.tile_width;
-                            renderable.h = data.tile_height;
-
-                        } else {
-                            console.warn("createRenderable with type TILE called with no data.");
-                        }
-
-                    } else {
-                        console.warn("createRenderable called with invalid type.");
-                    }
-
-                    renderables.push(renderable);
-
-                    return renderable;
-                };
-
-                this.render = function() {
-                    if(camera.bound_renderable) {
-                        camera.x = camera.bound_renderable.x;
-                        camera.y = camera.bound_renderable.y;
-                    }
-
-                    var ctx = canvas.getContext("2d");
-                    ctx.clearRect(0, 0, canvas.width, canvas.height);
-                    ctx.scale(camera.zoom, camera.zoom);
-
-                    for(var i in renderables) {
-                        var renderable = renderables[i];
-                        if(renderable) {
-                            if(renderable.color) {
-                                ctx.strokeStyle = renderable.color;
-                                ctx.fillStyle = renderable.color;
-                            }
-
-                            if(renderable.type === 'RECTANGLE') {
-                                ctx.fillRect(renderable.x - camera.x, renderable.y - camera.y, renderable.w, renderable.h);
-
-                            } else if(renderable.type === 'TEXT') {
-                                var font = "";
-                                if(renderable.fontSize) {
-                                    font += renderable.fontSize;
-                                } else {
-                                    font += "24px";
-                                }
-                                if(renderable.fontFamily) {
-                                    font += " " + renderable.fontFamily;
-                                } else {
-                                    font += " sans-serif";
-                                }
-                                ctx.font = font;
-                                ctx.fillText(renderable.text, renderable.x - camera.x, renderable.y - camera.y);
-
-                            } else if(renderable.type === 'TILE') {
-                                if(renderable.tileset && renderable.tileset.loaded) {
-                                    var tiles_in_row = Math.floor(renderable.tileset.width / renderable.tileset.tile_width);
-                                    var rows = Math.floor(renderable.tileset.height / renderable.tileset.tile_height);
-                                    var start_x = (renderable.sprite % tiles_in_row) * renderable.tileset.tile_width;
-                                    var start_y = (Math.floor(renderable.sprite / tiles_in_row) % rows) * renderable.tileset.tile_height;
-                                    ctx.drawImage(renderable.tileset.image, start_x, start_y, renderable.tileset.tile_width, renderable.tileset.tile_height, renderable.x - camera.x, renderable.y - camera.y, renderable.w, renderable.h);
-                                }
-
-                            } else { // unexpected renderable.type
-                                //ctx.moveTo(renderable.x, renderable.y);
-                            }
-                        }
-                    }
-
-                    ctx.stroke();
-                    ctx.scale(1/camera.zoom, 1/camera.zoom); // restore zoom
-                };
-
-                this.drawPauseScreen = function() {
-                    var ctx = canvas.getContext("2d");
-                    ctx.fillStyle = "rgb(0, 0, 0, 0.5)";
-                    ctx.fillRect(0, 0, canvas.width, canvas.height);
-                    ctx.fillStyle = "white";
-                    ctx.font = "20px sans-serif";
-                    ctx.fillText("Game Paused, Click To Continue . . .", 5, 20);
-                };
-
-                this.translateCamera = function(x, y) {
-                    camera.x += x;
-                    camera.y += y;
-                };
-
-                this.rotateCamera = function(deg) {
-                    camera.rotation += deg;
-                };
-
-                this.zoomCamera = function(scale) {
-                    camera.zoom *= scale;
-                };
-
-                this.bindCameraToRenderable = function(renderable) {
-                    camera.bound_renderable = renderable;
-                };
-            };
-        }();
-
         this.getWidth = function() {
             return canvas.width;
         };
@@ -173,7 +54,7 @@ var gameEngine = (function() {
 
         this.unpause = function() {
             paused = false;
-        }
+        };
 
         this.init = function(params) {
             /* Params:
